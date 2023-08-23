d2c556d959f9f715236746c63066335f1c03d47e
Savio-G <savio052003@gmail.com>
2023-08-23 13:37:00 +0600

    made chefs card and recipe card responsive

--- src/Components/Chefs/Chefs.jsx
+++ src/Components/Chefs/Chefs.jsx
@@ -5,24 +5,25 @@ const Chefs = ({ chef }) => {
   const { id, name, years_of_experience, image, noOfRecipes } = chef
   return (
 
-    <div className='lg:w-[40vw] h-[60vh] lg:h-[40vh] flex flex-col lg:flex-row md:flex-row rounded-[8px] overflow-hidden chef-shadow mb-[2rem]'>
+    <div className='lg:w-[40vw] h-[60vh] md:h-[50vh] lg:h-[40vh] flex flex-col lg:flex-row md:flex-row rounded-[8px] overflow-hidden chef-shadow mb-[2rem]'>
       <div className='bg-custom-color md:w-[40%] lg:w-[40%] w-[100%] h-[40%]  md:h-[100%] lg:h-[100%] chef-bg grid place-items-center'>
-        <img className='h-[100px] md:h-[200px] lg:h-[200px]' src="https://i.ibb.co/ZdN1gfG/dinner.png" alt="" />
+        <img className='h-[100px] md:h-[200px] lg:h-[125px] 2xl:h-[200px]' src="https://i.ibb.co/ZdN1gfG/dinner.png" alt="" />
 
       </div>
       <div className='w-[100%] md:w-[60%] lg:w-[60%] h-[60%] md:h-[100%] lg:h-[100%] bg-[#F1F0E8] p-[1rem] flex flex-col justify-between '>
         <div>
           <div className='flex justify-center'>
-            <img className='h-[100px] md:h-[150px] lg:h-[150px]' src={image} alt="" />
+            <img className='h-[100px] md:h-[150px] lg:h-[125px] 2xl:h-[150px]' src={image} alt="" />
           </div>
           <div className='text-center'>
-            <h1 className='text-2xl md:text-[4xl] lg:text-[4xl]'>{name}</h1>
+            <h1 className='text-xl md:text-[2xl] lg:text-[1.15rem] 2xl:text-2xl font-semibold'>{name}</h1>
             <div className='flex justify-evenly'>
               <div>
-                <p className=' text-xl md:text-[2xl] lg:text-[2xl]'>Recipes:{noOfRecipes}</p>
-                <p className='text-xl md:text-[2xl] lg:text-[2xl]'>Experience:{years_of_experience}</p>
+                <p className=' text-lg md:text-[2xl] lg:text-[1.10rem] 2xl:text-[1.30rem]'><span className='font-semibold'>Recipes:</span> {noOfRecipes}</p>
+                <p className='text-lg md:text-[2xl] lg:text-[1.10rem] 2xl:text-[1.30rem]'>
+                  <span className="font-semibold">Experience:</span> {years_of_experience}</p>
               </div>
-              <div className='flex items-center text-xl md:text-[2xl] lg:text-[2xl]'>
+              <div className='flex items-center text-lg md:text-[2xl] lg:text-[1.10rem] 2xl:text-[1.30rem]'>
                 <BiLike></BiLike>20
               </div>
             </div>
@@ -32,11 +33,11 @@ const Chefs = ({ chef }) => {
         </div>
         <div>
           <Link to={`/ChefDetail/${id}`}>
-            <button className='block w-[100%] bg-[#D4A373] text-black p-[0.25rem] rounded-[8px] md:p-[0.65rem] md:rounded-[10px]  lg:p-[0.65rem] lg:rounded-[10px]'>View Recipes</button>
+            <button className='block w-[100%] bg-[#D4A373] text-black p-[0.25rem] rounded-[8px] md:p-[0.65rem] md:rounded-[10px]  lg:p-[0.45rem] lg:rounded-[10px] font-semibold'>View Recipes</button>
           </Link>
         </div>
       </div>
-    </div>
+    </div >
 
 
 
--- src/Components/Chefs/RecipeCard.jsx
+++ src/Components/Chefs/RecipeCard.jsx
@@ -3,21 +3,19 @@ import React from 'react';
 const RecipeCard = ({ recipe }) => {
   const { recipe_name, ingredients, cooking_time, cooking_method, rating } = recipe
   return (
-    <div className='bg-[#EEE0C9] h-[70vh] lg:h-[75vh] w-[100%] lg:w-[23vw] mb-[2rem] p-9 flex flex-col justify-between chef-shadow rounded-xl'>
+    <div className='bg-[#EEE0C9] h-[55vh] md:h-[60vh] lg:h-[70vh] 2xl:h-[60vh] w-[100%] lg:w-[25vw] 2xl:w-[22vw] mb-[2rem] p-9 flex flex-col justify-between chef-shadow rounded-xl'>
       <div className=''>
-        <h1 className='text-[2rem] font-semibold text-center mb-8rem'>{recipe_name}</h1>
+        <h1 className='text-[1.75rem] sm:text-[2rem] lg:text-[1.75rem] 2xl:text-[2rem] font-semibold text-center mb-8rem'>{recipe_name}</h1>
         <>
-          <p className='text-[1.25rem] md:text-[1.5rem] lg:text-[1.25rem]'><span className='text-[#D4A373] text-[2rem]
-font-semibold'>1. </span><span className='font-medium'>Ingredients: </span>{ingredients}</p>
-          <p className='text-[1.25rem] md:text-[1.5rem] lg:text-[1.25rem]'><span className='text-[#D4A373] text-[2rem]
-          font-semibold'>2. </span><span className='font-medium'>Method: </span>{cooking_method}</p>
-          <p className='text-[1.25rem] md:text-[1.5rem] lg:text-[1.25rem]'><span className='text-[#D4A373] text-[2rem]
-          font-semibold'>3. </span><span className='font-medium'>Cooking time: </span>{cooking_time}</p>
-          <p className='text-[1.25rem] md:text-[1.5rem] lg:text-[1.25rem]'><span className='text-[#D4A373] text-[2rem]
-          font-semibold'>4. </span><span className='font-medium'>Rating: </span> {rating}</p>
+          <p className='text-[1rem] sm:text-[1.25rem] lg:text-[1.15rem] 2xl:text-[1.35rem]'><span className='text-[#D4A373]
+           text-[1.5rem] lg:text-[1.75rem] 2xl:text-[2rem] font-semibold'>1. </span><span className='font-medium'>Ingredients: </span>{ingredients}</p>
+          <p className='text-[1rem] sm:text-[1.25rem] lg:text-[1.15rem] 2xl:text-[1.35rem]'><span className='text-[#D4A373]
+           text-[1.5rem] lg:text-[1.75rem] 2xl:text-[2rem] font-semibold'>2. </span><span className='font-medium'>Method: </span>{cooking_method}</p>
+          <p className='text-[1rem] sm:text-[1.25rem] lg:text-[1.15rem] 2xl:text-[1.35rem]'><span className='text-[#D4A373] text-[1.5rem] lg:text-[1.75rem] 2xl:text-[2rem] font-semibold'>3. </span><span className='font-medium'>Cooking time: </span>{cooking_time}</p>
+          <p className='text-[1rem] sm:text-[1.25rem] lg:text-[1.15rem] 2xl:text-[1.35rem]'><span className='text-[#D4A373] text-[1.5rem] lg:text-[1.75rem] 2xl:text-[2rem] font-semibold'>4. </span><span className='font-medium'>Rating: </span> {rating}</p>
         </>
       </div>
-      <button className='bg-[#D4A373] text-black p-[0.65rem] rounded-[8px] md:p-[0.65rem] md:rounded-[10px]  lg:p-[0.65rem] lg:rounded-[10px]'>Add to Favourites</button>
+      <button className='bg-[#D4A373] text-black p-[0.40rem] rounded-[8px] md:p-[0.65rem] md:rounded-[10px]  lg:p-[0.65rem] lg:rounded-[10px] mt-[8px]'>Add to Favourites</button>
     </div>
   );
 };
